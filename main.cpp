3c214d4518e214b838c000a5e08d28c8843ea51a
Lev Pechersky <levpechersky@gmail.com>
2016-11-29 00:33:22 +0200

    Add missing main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * AVL.hpp
+ *
+ *  Created on: 2016-11-22
+ *      Author: Lev Pechersky
+ */
+#include <gtest/gtest.h>
+
+
+
+int main(int argc, char* argv[]) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
